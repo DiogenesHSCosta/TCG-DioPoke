d6a8be6445fd969589bf2b02260b022e28616e2f
Diogenes Costa <diogenes.henriquecosta@outlook.com>
2023-02-03 11:34:13 -0300

    criação dos middlewares

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,10 @@
 import Express from "express";
+import middlewares from "./Middlewares/config.js";
+import Cors from "cors";
 
 const app = Express()
+
+middlewares(app, Express, Cors)
+
 
 export default app
---
+++ src/Middlewares/config.js
@@ -0,0 +1,19 @@
+const middlewares = (app, Express, cors)=>{
+
+    //Convertendo a entrada em json com o comando '.json()'
+    app.use(Express.json());
+
+    //Impedir gargalo. Garantir que as requisições vão ser realizadas sequencialmente
+    app.use((request, response, next) => {
+      next();
+    });
+  
+    //Impedir que outros https, tirando o nosso, acessem a API
+    const options ={
+      origin: ["*"]
+    }
+  
+    app.use(cors(options))
+}
+
+export default middlewares
