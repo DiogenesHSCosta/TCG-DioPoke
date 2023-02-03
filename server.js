06aa6f9317ab82112cb7b451a0d223b45bd9650a
Diogenes Costa <diogenes.henriquecosta@outlook.com>
2023-02-03 11:04:45 -0300

    criação do arquivo server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+import app from "./src/main.js";
+
+const port = 3000
+app.listen(port, () =>{
+    console.log(`API rodando na porta: ${port}`)
+})
---
+++ src/Main.js
@@ -0,0 +1,5 @@
+import Express from "express";
+
+const app = Express()
+
+export default app
